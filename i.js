b522efd8e574224b0bc610bb0da293612459b1e4
Veer5487 <ritiksingh0090@gmail.com>
2025-08-01 12:19:12 +0530

    Add files via upload

---
+++ i.js
@@ -0,0 +1,24 @@
+    // let navLinks1=document.getElementById("navLinks");
+    // function menu() {
+    //   let navLinks = document.getElementById('navLinks1');
+    //   navLinks.classList.toggle('active');
+    // }
+    // let menu=document.getElementById("menu")
+    // let navLinks1 = document.getElementById('navLinks1');
+    // let br=!br
+    // function menu(){
+    //   if(br===br){
+    //     navLinks1.style.display="inline"
+    //     navLinks1.style.transform="translateX(0px);"
+
+    //   }
+    //   else{
+    //     navLinks1.style.transform="translateX(-500px);"
+    //   }
+    //   br=!br
+    // }
+
+    function toggleMenu() {
+      const navLinks = document.getElementById('navLinks');
+      navLinks.classList.toggle('active');
+    }
